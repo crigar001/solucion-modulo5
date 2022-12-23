8ec26efca5b8295945a65ee229bfc2fb4241cfd8
crigar001 <crigar001@gmail.com>
2022-12-22 23:03:33 -0500

    cambios repositorio drumpsterkitter

--- js/script.js
+++ js/script.js
@@ -15,11 +15,11 @@ var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
-  "https://davids-restaurant.herokuapp.com/categories.json";
+  "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
-  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
@@ -83,9 +83,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML,
-  true); // Explicitely setting the flag to get JSON from server processed into an object literal
+  buildAndShowHomeHTML, 
+  true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
+
 // *** finish **
 
 
@@ -115,16 +116,14 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = homeHtml
-        .replace(new RegExp("{{randomCategoryShortName}}", "g"),
-              "'" + chosenCategoryShortName + "'");
 
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "short_name", "' " + chosenCategoryShortName + "' ");
 
-      // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
+      // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
+      // ....
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
@@ -154,7 +153,7 @@ dc.loadMenuCategories = function () {
 dc.loadMenuItems = function (categoryShort) {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
-    menuItemsUrl + categoryShort,
+    menuItemsUrl + categoryShort + '.json',
     buildAndShowMenuItemsHTML);
 };
 
